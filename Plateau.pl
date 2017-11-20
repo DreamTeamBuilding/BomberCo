e2ce7da46b2183b69ad241fe9fa1ccd81d278bc7
unknown <rgouttefan@IF501-210-17.insa-lyon.fr>
2017-11-20 11:15:28 +0100

    Plateau

---
+++ Plateau.pl
@@ -0,0 +1,45 @@
+:- dynamic board/1.
+
+creerPlateau(TailleCote):-
+	TaillePlateau is TailleCote * TailleCote,
+	length(Board, TaillePlateau),
+	assert(board(Board)),
+	fill(Board, NewBoard, TailleCote),
+	applyIt(Board, NewBoard),
+	displayBoard(TailleCote).	
+	
+	
+displayBoard(TailleCote):-
+	writeln('-----------'),
+	printBoard(TailleCote, 0),
+	writeln('-----------'), nl.
+	
+printBoard(TailleCote, TailleCote).
+printBoard(TailleCote, Index):-
+	printLine(Index, TailleCote, 0),
+	IndexSuiv is Index+1,
+	printBoard(TailleCote, IndexSuiv).
+	
+printLine(Line, TailleCote, TailleCote):-nl.
+printLine(Line, TailleCote, IndexInLine):-
+	Index is Line*TailleCote + IndexInLine,
+	printVal(Index),
+	NextIndex is IndexInLine+1,
+	printLine(Line, TailleCote, NextIndex).
+
+printVal(N) :- board(B), nth0(N,B,Val), var(Val), write(' '), !.
+printVal(N) :- board(B), nth0(N,B,Val), write(Val).
+
+fill(Board, NewBoard, TailleCote):-
+	fillLine(Board, NewBoard, TailleCote, 0, 0).
+
+fillLine(Board, NewBoard, TailleCote, Line, TailleCote).
+fillLine(Board, NewBoard, TailleCote, Line, Index):- 
+	Board = NewBoard,
+	Current is TailleCote*Line+Index, 
+	nth0(Current, NewBoard, 'X'),
+	printVal(Current),
+	IndexSuiv is Index+1,
+	fillLine(Board, NewBoard, TailleCote, Line, IndexSuiv).
+	
+applyIt(Board,NewBoard) :- retract(board(Board)), assert(board(NewBoard)).
