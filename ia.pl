9aa414d70b2a94c6d6c6b6c79f78574de05d6e07
unknown <vbonin@IF501-210-16.insa-lyon.fr>
2017-11-20 11:03:33 +0100

    Pitite correction EZ

--- ia.pl
+++ ia.pl
@@ -1,16 +1,24 @@
 %%%% Artificial intelligence: choose in a Board the Move to play for Player (_)
 %%%% This AI plays more or less randomly according to the version and does not care who is playing:
-Index(1,-11).	% haut
-Index(2,11).	% bas
-Index(3,1).		% droite
-Index(4,-1).  	% gauche
-Index(5,0).		% immobile
-Index(6,0).		% bombe
+index(1,-11).	% haut
+index(2,11).	% bas
+index(3,1).	% droite
+index(4,-1).	% gauche
+index(5,0).	% immobile
+index(6,0).	% bombe
 
-% iav1 : fais tout de manière random
-ia(Board, PosIndex, Move, iav1) :- repeat, Move is random(7), Index(Move, I), NewPosIndex is PosIndex+I, nth0(NewPosIndex, Board, Occupe), not(var(Occupe)), !.
+posSafe(Index, Board) :-
 
-% iav2 : détecte les zones de danger des bombes et bouge de manière random tant qu'elle n'est pas sortie
-ia(Board, PosIndex, Move, iav2) :- repeat, (posSafe(PosIndex, Board) -> repeat, Move is random(7),Index(Move, I), NewPosIndex is PosIndex+I, posSafe(NewPosIndex, Board); Move is random(5),Index(Move, I), NewPosIndex is PosIndex+I), nth0(NewPosIndex, Board, Occupe), not(var(Occupe)), !.
 
-% iav3 : détecte les zones de danger et cherche à s'en éloigner
+% iav1 : fait tout de maniere random
+ia(Board, PosIndex, Move, iav1) :- repeat, Move is random(7), index(Move, I), NewPosIndex is PosIndex+I, nth0(NewPosIndex, Board, Occupe), not(var(Occupe)), !.
+
+% iav2 : detecte les zones de danger des bombes et bouge de maniere random tant qu'elle n'est pas sortie
+ia(Board, PosIndex, Move, iav2) :- repeat, (posSafe(PosIndex, Board) -> repeat, Move is random(7),index(Move, I), NewPosIndex is PosIndex+I, posSafe(NewPosIndex, Board); Move is random(5),index(Move, I), NewPosIndex is PosIndex+I), nth0(NewPosIndex, Board, Occupe), not(var(Occupe)), !.
+
+% iav3 : detecte les zones de danger et cherche a s'en eloigner
+ia(Board, PosIndex, Move, iav3):-
+    repeat, (posSafe(PosIndex, Board) ->
+            repeat, Move is random(7),index(Move, I), NewPosIndex is PosIndex+I, posSafe(NewPosIndex, Board);
+            Move is random(5),index(Move, I), NewPosIndex is PosIndex+I),
+    nth0(NewPosIndex, Board, Occupe), not(var(Occupe)), !.
