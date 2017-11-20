911e977de6d2cdc1a82584425d056e88d80329cd
Lucas OH <luky.herbin@free.fr>
2017-11-20 19:47:06 +0100

    [LOH] Ajout de quelques TODO pour pas zapper de trucs :)

--- joueurs.pl
+++ joueurs.pl
@@ -1,16 +1,13 @@
 %%%%% Positionne les joueurs dans les coins du plateau
 initJoueurs(NbJoueurs, TaillePlateau):-
-
 	nbJoueurs(NbJoueurs),
 	(not(joueursSav(_,_)) ; retractall(joueursSav(_,_))),
-	
 	(NbJoueurs < 5,NbJoueurs >1),
+
 	Position is TaillePlateau +1, assert(joueursSav(Position,-1)), 
 	Position2 is TaillePlateau*TaillePlateau-TaillePlateau-2, assert(joueursSav(Position2,-1)), 
 	(NbJoueurs < 3 ; (Position3 is TaillePlateau*2-2), assert(joueursSav(Position3,-1))),
 	(NbJoueurs < 4 ; (Position4 is TaillePlateau*TaillePlateau-TaillePlateau*2+1, assert(joueursSav(Position4,-1)))).
+
 	
-	
-compteSurvivants(0,[]).
-compteSurvivants(NbVivants,[X|Joueurs]):- compteSurvivants(NbVivantsPrec,Joueurs),
-	(var(X) -> NbVivants is NbVivantsPrec +1; NbVivants is NbVivantsPrec).
+plusieursEnVie:-joueursSav(X,-1),joueursSav(Y,-1),Y\==X.
