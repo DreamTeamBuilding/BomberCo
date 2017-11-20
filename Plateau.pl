7c3530dc5785bf279cb5f176441b1aebc5788438
unknown <rgouttefan@IF501-210-17.insa-lyon.fr>
2017-11-20 11:58:20 +0100

    Murs

--- Plateau.pl
+++ Plateau.pl
@@ -9,7 +9,7 @@ creerPlateau(TailleCote):-
 	displayBoard(TailleCote),
 	retract(board(Board)).	
 	
-	
+% Affichage du plateau
 displayBoard(TailleCote):-
 	writeln('-----------'),
 	printBoard(TailleCote, 0),
@@ -31,15 +31,44 @@ printLine(Line, TailleCote, IndexInLine):-
 printVal(N) :- board(B), nth0(N,B,Val), var(Val), write(' '), !.
 printVal(N) :- board(B), nth0(N,B,Val), write(Val).
 
+
+% Remplissage du plateau
 fill(Board, NewBoard, TailleCote):-
-	fillLine(Board, NewBoard, TailleCote, 0, 0).
+	fillLine(Board, NewBoard, TailleCote, 0, 0),
+	fillColumn(Board, NewBoard, TailleCote, 1, 0),
+	fillColumn(Board, NewBoard, TailleCote, 1, TailleCote-1),
+	fillLine(Board, NewBoard, TailleCote, TailleCote-1, 0).
+	%fillObstacles(Board, NewBoard, TailleCote, 0, 0).
 
 fillLine(Board, NewBoard, TailleCote, Line, TailleCote).
 fillLine(Board, NewBoard, TailleCote, Line, Index):- 
 	Board = NewBoard,
 	Current is TailleCote*Line+Index, 
-	nth0(Current, NewBoard, 'X'),
+	nth0(Current, NewBoard, 1),
 	IndexSuiv is Index+1,
 	fillLine(Board, NewBoard, TailleCote, Line, IndexSuiv).
 	
+	
+fillColumn(Board, NewBoard, TailleCote, TailleCote, Column).
+fillColumn(Board, NewBoard, TailleCote, Index, Column):- 
+	Board = NewBoard,
+	Current is TailleCote*Index+Column, 
+	nth0(Current, NewBoard, 1),
+	IndexSuiv is Index+1,
+	fillColumn(Board, NewBoard, TailleCote, IndexSuiv, Column).
+	
+%fillObstacles(Board, NewBoard, TailleCote, TailleCote, TailleCote).
+%fillObstacles(Board, NewBoard, TailleCote, Index, TailleCote):-
+%	IndexSuiv is Index+2,
+%	fillObstacles(Board, NewBoard, TailleCote, IndexSuiv, 0).
+%fillObstacles(Board, NewBoard, TailleCote, Line, Index):- 
+%	Board = NewBoard,
+%	Current is TailleCote*Line+Index, 
+%	nth0(Current, NewBoard, 1),
+%	IndexSuiv is Index+1,
+%	print(IndexSuiv),
+%	fillObstacles(Board, NewBoard, TailleCote, Line, IndexSuiv).
+
+	
+% 
 applyIt(Board,NewBoard) :- retract(board(Board)), assert(board(NewBoard)).
