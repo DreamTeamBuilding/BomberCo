4c70119eea4d3e1e25be9dc78f4b6fdf64930a35
Lucas OH <luky.herbin@free.fr>
2017-11-20 10:12:03 +0100

    [LOH] Initialisation du jeu et placement des joueurs

--- bomberCo.pl
+++ bomberCo.pl
@@ -1,3 +1,29 @@
 :- dynamic board/1.
-:- dynamic player1/1.
-:- dynamic player2/1.
+:- dynamic players/1.
+
+%play(IndexPlayer).
+
+
+%%%%% Start !
+init(NbPlayers, TaillePlateau) :-
+    %Initialisation du plateau
+    length(Board,Taille),
+    Taille is TaillePlateau * TaillePlateau,
+    assert(board(Board)),
+    %Initialisation Player
+    (initPlayers(NbPlayers, TaillePlateau) ; writeln("Erreur lors de l'initialisation des joueurs")).
+
+initPlayers(NbPlayers, TaillePlateau):-
+    length(Players,NbPlayers),      
+	(NbPlayers < 5,NbPlayers >1),
+	nth0(0, Players, Position), Position is TaillePlateau +1,
+	nth0(1, Players, Position2), Position2 is TaillePlateau*TaillePlateau-TaillePlateau-2,
+	(NbPlayers < 3 ; (nth0(2, Players, Position3), Position3 is TaillePlateau*2-2)),
+	(NbPlayers < 4 ; (nth0(3, Players, Position4), Position4 is TaillePlateau*TaillePlateau-TaillePlateau*2+1)),
+    print(Players),
+    assert(players(Players)).
+
+
+
+print([]).
+print([X|T]):- write(X),write(','),print(T).
