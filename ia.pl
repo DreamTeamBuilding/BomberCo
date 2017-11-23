4eac7626e8c6be77a2ccdcf81262476d51f79709
Victor Bonin <victorduvercors@gmail.com>
2017-11-23 02:19:11 +0100

    Victor : Correction bug iav1

--- ia.pl
+++ ia.pl
@@ -29,23 +29,30 @@ isSafe(Pos, Plateau) :-  % la case a l'index Pos est safe ?
     (not(bombes(Pos+(2*TaillePlateau), Temps)); Temps < 3; ((nth0(Pos+TaillePlateau, Plateau, Case), Case==1))),
     (not(bombes(Pos-(2*TaillePlateau), Temps)); Temps < 3; ((nth0(Pos-TaillePlateau, Plateau, Case), Case==1))).
 
-isPossible(NewPos, Board) :- not(bombes(NewPos,_)), not(joueursSav(_,NewPos,-1)), nth0(NewPos, Board, 0).
 isPossible(FormerPos,NewPos, Board) :- not(bombes(NewPos,_)), (not(joueursSav(_,NewPos,-1));FormerPos==NewPos), nth0(NewPos, Board, 0).
 
+% Liste des positions adjacentes a Pos
 posAdjacentes(Pos, [Haut, Gauche, Droite, Bas]) :- taillePlateau(TaillePlateau), Haut is Pos-TaillePlateau, Gauche is Pos-1, Droite is Pos+1, Bas is Pos + TaillePlateau.
 
-posAdjacentesPossibles(_,[],_).
-posAdjacentesPossibles(Board, [X|L], PAP) :- posAdjacentesPossibles(Board, L, NewPAP), append(PAP,[X],NewPAP), isPossible(X, Board).
-posAdjacentesPossibles(Board, [_|L], PAP) :- posAdjacentesPossibles(Board, L, PAP).
+% Liste des positions accessibles depuis Pos
+posSuivantes(Pos, PositionsSuivantes) :- append(posAdjacentes(Pos,_),Pos,PositionsSuivantes).
 
-posAdjacentesSafe([],_,_). % condition de sortie : on a teste toutes les positions adjacentes possibles
-posAdjacentesSafe([X|ListeIndex],Plateau, PosSafes) :- posAdjacentesSafe(ListeIndex,Plateau,NewPosSafes),isSafe(X,Plateau), append(PosSafes, [X], NewPosSafes). % la position adjacente est safe
-posAdjacentesSafe([_|ListeIndex],Plateau, PosSafes) :- posAdjacentesSafe(ListeIndex,Plateau, PosSafes). % la position adjacente n'est pas safe
+% Liste des positions realisables depuis FormerPos (pas d'obstacle)
+posSuivantesPossibles(_,_,[],_).
+posSuivantesPossibles(Board, FormerPos,[X|ListePos], PosSuivantesPossibles) :- PosSuivantesPossibles(Board, FormerPos, ListePos, NewPAP), append(PosSuivantesPossibles,[X],NewPAP), isPossible(FormerPos, X, Board).
+posSuivantesPossibles(Board, FormerPos, [_|L], PAP) :- posSuivantesPossibles(Board, FormerPos, L, PAP).
 
+% Liste des positions safe
+posSuivantesSafe([],_,_).
+posSuivantesSafe([X|ListeIndex],Plateau, PosSafes) :- posSuivantesSafe(ListeIndex,Plateau,NewPosSafes),isSafe(X,Plateau), append(PosSafes, [X], NewPosSafes). % la position est safe
+posSuivantesSafe([_|ListeIndex],Plateau, PosSafes) :- posSuivantesSafe(ListeIndex,Plateau, PosSafes). % la position n'est pas safe
 
+posAdjacentesPlusProches(Pos, PositionsAdjacentes, MeilleurMouvement).
 
 % iav1 : fait tout de maniere random
-ia(Plateau, PosIndex, NewPosIndex, BombePosee, iav1) :- repeat, Move is random(7), indexAction(Move, I, BombePosee), NewPosIndex is PosIndex+I, isPossible(PosIndex, NewPosIndex, Plateau), !.
+ia(Plateau, PosIndex, NewPosIndex, BombePosee, iav1) :- posSuivantes(PosIndex, PositionsSuivantes), posSuivantesPossibles(Plateau, PosIndex, PositionsSuivantes, PosSuivantesPossibles),
+	 (length(PosSuivantesPossibles,0) -> NewPosIndex is PosIndex, BombePosee is 0;
+	 repeat, Move is random(7), indexAction(Move,I,BombePosee), NewPosIndex is PosIndex+I),isPossible(PosIndex, NewPosIndex, Plateau), !.
 
 % Iav2 : Detecte et evite les zones de danger des bombes et bouge de
 % maniere random tant qu'elle n'est pas sortie
@@ -60,26 +67,28 @@ ia(Board, PosIndex, NewPosIndex, BombePosee, iav2) :-
 ia(Board, PosIndex, NewPosIndex,BombePosee, iav3) :-
     repeat, (isSafe(PosIndex, Board) ->
             repeat, Move is random(7),indexAction(Move, MvmtRelatif, BombePosee), NewPosIndex is PosIndex+MvmtRelatif, isSafe(NewPosIndex, Board), isPossible(PosIndex, NewPosIndex, Board),!; % Si en dehors de zone de danger : random
-            posAdjacentes(PosIndex, PosAdjacentes), posAdjacentesPossibles(Board, PosAdjacentes, PosAdjacentesPossibles),
-	    posAdjacentesSafe(PosAdjacentesPossibles, Board, PosAdjacentesSafes),
-	     % si PosAdjacentesSafes est vide : piocher dans PosAdjacentesPossibles
-	     ((length(PosAdjacentesPossibles,0)) ->
-	     random_member(NewPosIndex, PosAdjacentes);
-	     random_member(NewPosIndex, PosAdjacentesSafes))),
+            posAdjacentes(PosIndex, PosAdjacentes), posSuivantesPossibles(Board,PosIndex, PosAdjacentes, PosSuivantesPossibles),
+	    posSuivantesSafe(PosSuivantesPossibles, Board, PosSuivantesSafes),
+	     % si PosSuivantesSafes est vide : piocher dans PosSuivantesPossibles
+	     ((length(PosSuivantesSafes,0)) ->
+	     random_member(NewPosIndex, PosSuivantesPossibles);
+	     random_member(NewPosIndex, PosSuivantesSafes))),
     !.
 
-% Non fonctionnelle !
+
 % iav4 : se rapproche de l'adversaire pour poser des bombes avec les
 % fonctionnalites precedentes
 ia(Board, PosIndex, NewPosIndex,BombePosee, iav4) :-
     repeat, (isSafe(PosIndex, Board) ->
 	    % si Safe :
 	    (joueursSav(_,PosJoueurs,-1), adversairePlusProche(PosIndex, PosJoueurs, DistanceVolOiseau), DistanceVolOiseau =< 3 ->
-		% si loin de l'adversaire le + proche : random mais essaye de s'approcher
-	    repeat, Move is random(10*(4-DistanceVolOiseau)), (Move > 6 -> Move = 6), indexAction(Move,MvmtRelatif,BombePosee), NewPosIndex is PosIndex+MvmtRelatif, isPossible(PosIndex, NewPosIndex,Board), isSafe(NewPosIndex,Board),!;
-	    % si proche de l'adversaire : random mais a + de chances de poser des bombes
-
-	    a),
+		% si proche de l'adversaire le + proche : random mais a plus de chances de bomber
+	    repeat, Move is random(10*(4-DistanceVolOiseau)), (Move > 6 -> Move = 6; true), indexAction(Move,MvmtRelatif,BombePosee), NewPosIndex is PosIndex+MvmtRelatif, isPossible(PosIndex, NewPosIndex,Board), isSafe(NewPosIndex,Board),!;
+	    % si loin de l'adversaire le + proche : random mais essaye de s'approcher
+	    posSuivantes(PosIndex, PosSuivantes),
+	    posSuivantesPossibles(Board, PosIndex, PosSuivantes, PosSuivantesPossibles),
+	    posSuivantesSafe(PosSuivantesPossibles, Board, PosSuivantesSafes),
+	    posAdjacentesPlusProches(PosIndex, PosSuivantesSafes, MeilleurMouvement)),
 
 	    % Si dans zone de danger : on regarde quelles positions adjacentes sont safe
             posAdjacentes(PosIndex, PosAdjacentes), posAdjacentesPossibles(Board, PosAdjacentes, PosAdjacentesPossibles),
