18c30839203eabae2506d74f275fb6f1c6c3222e
Romain <Romain@DESKTOP-MLAOIJC>
2017-11-26 15:36:45 +0100

    Tests non fonctionnels, raison inconnue...

--- tests.pl
+++ tests.pl
@@ -1,6 +1,7 @@
 :-begin_tests(plateau).
 	test(initPlateau):-
-		initPlateau(11),
+		assert(taillePlateau(11)),
+		initPlateau,
 		taillePlateau(11),
 		plateauSav(B),
 		length(B, 121),
@@ -11,25 +12,24 @@
 		PlateauC = ([1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1]),
 		comp(Plateau, PlateauC),
 		!.
-	test(writeVal):-
-		writeVal(0),
-		writeVal(1),
-		writeVal(_),
-		!.
-:- end_tests(plateau).
+:-end_tests(plateau).
 
 comp(L1, L1).
 
 
 :-begin_tests(joueurs).
 	test(initJoueurs):-
-		initJoueurs(2, 11),
+		assert(nbJoueurs(2)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		nbJoueurs(2),
 		joueursSav(_, 12, -1), 
 		joueursSav(_, 108, -1),
 		!.
 	test(initJoueurs):-
-		initJoueurs(4, 11),
+		assert(nbJoueurs(4)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		nbJoueurs(4),
 		joueursSav(_, 12, -1),  
 		joueursSav(_, 20, -1),
@@ -37,14 +37,18 @@ comp(L1, L1).
 		joueursSav(_, 108, -1),
 		!.
 	test(initJoueurs):-
-		initJoueurs(3, 11),
+		assert(nbJoueurs(3)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		nbJoueurs(3),
 		joueursSav(_, 12, -1),  
 		joueursSav(_, 20, -1),  
 		joueursSav(_, 108, -1),
 		!.
 	test(plusieursEnVie):-
-		initJoueurs(2, 11),
+		assert(nbJoueurs(2)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		plusieursEnVie,
 		retract(joueursSav(_, 108, _)),
 		assert(joueursSav(_, 108, 0)),
@@ -52,26 +56,46 @@ comp(L1, L1).
 		not(plusieursEnVie),
 		!.
 	test(actualiserJoueur):-
-		initJoueurs(2, 11),
+		assert(nbJoueurs(2)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		actualiserJoueur(1, 13),
 		joueursSav(_, 13, -1),
 		!.
 	test(joueurSuivant):-
-		initJoueurs(2,11),
+		assert(nbJoueurs(2)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		joueurSuivant(0,1),
 		joueurSuivant(1,0),
 		!.
 	test(joueurSuivant):-
-		initJoueurs(3,11),
+		assert(nbJoueurs(3)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		joueurSuivant(0,1),
 		joueurSuivant(1,2),
 		joueurSuivant(2,0),
+		!.
+	test(tuer):-
+		assert(nbJoueurs(2)),
+		assert(taillePlateau(11)),
+		initJoueurs,
+		tuer(0),
+		joueursSav(0, _, 0),
+		tuer(1),
+		joueursSav(1, _, 0),
+		!.
+	test(exploserBombe):-
+		assert(taillePlateau(11)),
+		%TODO
 		!.
 :-end_tests(joueurs).
 
 :-begin_tests(ia).
 	test(initIndex):-
-		initIndex(11),
+		assert(taillePlateau(11)),
+		initIndex,
 		indexAction(1,-TaillePlateau,0),
 		indexAction(2, TaillePlateau,0),
 		indexAction(3,1,0),
@@ -80,7 +104,8 @@ comp(L1, L1).
 		indexAction(6,0,1),
 		!.
 	test(distance):-
-		initPlateau(11),
+		assert(taillePlateau(11)),
+		initIndex,
 		ia:distance(12, 12, 0),
 		ia:distance(12, 13, 1),
 		ia:distance(12, 14, 2),
@@ -91,15 +116,13 @@ comp(L1, L1).
 		ia:distance(23, 12, 1), % Entre deux cases l'une sur l'autre
 		ia:distance(12, 27, 5), % Entre deux cases en diagonale
 		!.
-	/* test(isSafe):-
-		initPlateau(11),
-		plateauSav(B),
-		isSafe(13, B),
-		!. */
 	test(isPossible):-
-		initPlateau(11),
+		assert(taillePlateau(11)),
+		initIndex,
 		plateauSav(B),
-		initJoueurs(2, 11),
+		assert(nbJoueurs(2)),
+		assert(taillePlateau(11)),
+		initJoueurs,
 		joueursSav(0, 12, _),
 		isPossible(12, 13, B),
 		isPossible(12, 12, B),
@@ -107,15 +130,48 @@ comp(L1, L1).
 		not(isPossible(12, 11, B)),
 		not(isPossible(12, 1, B)),
 		!.
-		
-	test(isPossible):-
-		initPlateau(11),
-		plateauSav(B),
-		initJoueurs(2, 11),
-		joueursSav(0, 12, _),
-		isPossible(13, B),
-		isPossible(23, B),
-		not(isPossible(11, B)),
-		not(isPossible(1, B)),
-		!.
 :-end_tests(ia).
+
+:-begin_tests(bombes).
+	test(initBombes):-
+		initBombes,
+		not(bombes(_,_)),
+		assert(nbJoueurs(2)),
+		ajouterBombe(12),
+		initBombes,
+		not(bombes(_,_)),
+		!.
+	test(ajouterBombe):-
+		initBombes,
+		assert(nbJoueurs(2)),
+		ajouterBombe(14),
+		nbJoueurs(NbJoueurs),
+		Duree is NbJoueurs * 5,
+		bombes(14, Duree),
+		initBombes,
+		!.
+	test(decrementerBombes):-
+		initBombes,
+		assert(nbJoueurs(2)),
+		ajouterBombe(14),
+		ajouterBombe(23),
+		decrementerBombes,
+		bombes(14, 9),
+		bombes(23, 9),
+		decrementerBombes,
+		bombes(14, 8),
+		bombes(23, 8),
+		decrementerBombes,
+		decrementerBombes,
+		decrementerBombes,
+		decrementerBombes,
+		decrementerBombes,
+		decrementerBombes,
+		decrementerBombes,
+		decrementerBombes,
+		decrementerBombes, % On décrémente 8 fois pour faire exploser la bombe et vérifier si elle est toujours en mémoire.
+		not(bombes(14, _)),
+		not(bombes(23, _)),
+		initBombes,
+		!.
+:-end_tests(bombes).
